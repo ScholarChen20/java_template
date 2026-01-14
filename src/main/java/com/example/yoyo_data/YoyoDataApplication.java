67d876b1df09b16249651058425b6a46ae04a658
chenwenqin <c13599616147@163.com>
2026-01-14 16:19:38 +0800

    first commit

---
+++ src/main/java/com/example/yoyo_data/YoyoDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.yoyo_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoyoDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoyoDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yoyo_data/YoyoDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yoyo_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoyoDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
